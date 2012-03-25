632b44c58a891c8a9c054f19c076a03840c239d9
BetfairTrader <anton.zem@gmail.com>
2012-03-25 22:32:07 +0400

    keepAlive experiments

--- test/session.js
+++ test/session.js
@@ -1,5 +1,10 @@
 var fs = require('fs');
+var util = require('util');
+var https = require('https');
+
 var account = JSON.parse(fs.readFileSync("../account.json"));
+
+https.globalAgent.maxSockets = 10;
 
 account.login = account.login || "nobody";
 account.password = account.password || "password";
@@ -8,17 +13,35 @@ var betfairSport = require("../index.js");
 var session = betfairSport.openSession(account.login, account.password);
 
 session.on("error", function(res) {
-    console.log("error:");
-    console.log(res);
-    process.exit(1);
+	console.log("error:");
+	console.log(res);
+	process.exit(1);
 });
 
 session.on("loggedIn", function(res) {
-    console.log("got loggedIn event");
-    session.close();
+	var fastest = Infinity;
+	var slowest = 0;
+	var okCalls = 0;
+	console.log("got loggedIn event");
+	for ( var cnt = 0; cnt < 5; ++cnt) {
+		session.keepAlive(function keepAliveResult(res) {
+			console.log(res.result);
+			if (res.duration() < fastest)
+				fastest = res.duration();
+			if (res.duration() > slowest)
+				slowest = res.duration();
+			if (res.result.header.errorCode === "OK")
+				++okCalls;
+			if (okCalls === 5)
+				console.log(util.format(
+						"Ok calls:%s fastest:%sms slowest:%sms", okCalls,
+						fastest, slowest));
+		});
+	}
+	//session.close();
 });
 
 session.on("loggedOut", function(res) {
-    console.log("got loggedOut event");
-    process.exit(1);
+	console.log("got loggedOut event");
+	process.exit(1);
 });
