05c0da24c2748037b7c7714dd1ee4d2d432e7b7b
Anton Zemlyanov <anton.zem@gmail.com>
2012-03-26 09:56:32 +0400

    formatting fix

--- test/session.js
+++ test/session.js
@@ -13,35 +13,35 @@ var betfairSport = require("../index.js");
 var session = betfairSport.openSession(account.login, account.password);
 
 session.on("error", function(res) {
-	console.log("error:");
-	console.log(res);
-	process.exit(1);
+    console.log("error:");
+    console.log(res);
+    process.exit(1);
 });
 
 session.on("loggedIn", function(res) {
-	var fastest = Infinity;
-	var slowest = 0;
-	var okCalls = 0;
-	console.log("got loggedIn event");
-	for ( var cnt = 0; cnt < 5; ++cnt) {
-		session.keepAlive(function keepAliveResult(res) {
-			console.log(res.result);
-			if (res.duration() < fastest)
-				fastest = res.duration();
-			if (res.duration() > slowest)
-				slowest = res.duration();
-			if (res.result.header.errorCode === "OK")
-				++okCalls;
-			if (okCalls === 5)
-				console.log(util.format(
-						"Ok calls:%s fastest:%sms slowest:%sms", okCalls,
-						fastest, slowest));
-		});
-	}
-	//session.close();
+    var fastest = Infinity;
+    var slowest = 0;
+    var okCalls = 0;
+    console.log("got loggedIn event");
+    for ( var cnt = 0; cnt < 5; ++cnt) {
+        session.keepAlive(function keepAliveResult(res) {
+            console.log(res.result);
+            if (res.duration() < fastest)
+                fastest = res.duration();
+            if (res.duration() > slowest)
+                slowest = res.duration();
+            if (res.result.header.errorCode === "OK")
+                ++okCalls;
+            if (okCalls === 5)
+                console.log(util.format(
+                        "Ok calls:%s fastest:%sms slowest:%sms", okCalls,
+                        fastest, slowest));
+        });
+    }
+    //session.close();
 });
 
 session.on("loggedOut", function(res) {
-	console.log("got loggedOut event");
-	process.exit(1);
+    console.log("got loggedOut event");
+    process.exit(1);
 });
