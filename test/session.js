06f8b55fe6f40c132923570aefeba1c9960e6fe7
Anton Zemlyanov <anton.zem@gmail.com>
2012-03-27 07:49:45 +0400

    minor refactoring and cleanup

--- test/session.js
+++ test/session.js
@@ -1,51 +1,71 @@
+var https = require('https');
 var fs = require('fs');
 var util = require('util');
-var https = require('https');
+var async = require('async')
 
-var account = JSON.parse(fs.readFileSync("../account.json"));
+var account = JSON.parse(fs.readFileSync('../account.json'));
 
 https.globalAgent.maxSockets = 5;
 
-account.login = account.login || "nobody";
-account.password = account.password || "password";
+account.login = account.login || 'nobody';
+account.password = account.password || 'password';
 
-var betfairSport = require("../index.js");
+var betfairSport = require('../index.js');
 var session = betfairSport.openSession(account.login, account.password);
 
-session.on("error", function(res) {
-    console.log("error:");
-    console.log(res);
-    process.exit(1);
-});
-
-session.on("loggedIn", function(res) {
-    var fastest = Infinity;
-    var slowest = 0;
-    var okCalls = 0;
-    var totalCalls = 0;
-    console.log("got loggedIn event");
-    for ( var cnt = 0; cnt < 10; ++cnt) {
-        session.keepAlive(function keepAliveResult(res) {
-            console.log(res.result);
-            if (res.duration() < fastest)
-                fastest = res.duration();
-            if (res.duration() > slowest)
-                slowest = res.duration();
-            if (res.result.header.errorCode === "OK")
-                ++okCalls;
-            if (okCalls === 10)
-            {
-                console.log(util.format(
-                        "Ok calls:%s fastest:%sms slowest:%sms", okCalls,
-                        fastest, slowest));
-                        session.close();
+async.series({
+    // login
+    login : function(cb) {
+        console.log("logging in...");
+        session.open(function onLoginFinished(err, res) {
+            if (err) {
+                console.log("login error", err);
+                process.exit(-1);
             }
+            console.log("login ok");
+            cb(err, res);
         });
+    },
+
+    // send a number of keepAlive
+    sendKeepAlives : function(cb) {
+
+    },
+
+    // logout from Betfair
+    logout : function(cb) {
+
     }
-    //session.close();
 });
 
-session.on("loggedOut", function(res) {
-    console.log("got loggedOut event");
-    process.exit(1);
-});
+//session.on("loggedIn", function(res) {
+//    var fastest = Infinity;
+//    var slowest = 0;
+//    var okCalls = 0;
+//    var totalCalls = 0;
+//    console.log("got loggedIn event");
+//    for ( var cnt = 0; cnt < 10; ++cnt) {
+//        session.keepAlive(function keepAliveResult(res) {
+//            console.log(res.result);
+//            if (res.duration() < fastest)
+//                fastest = res.duration();
+//            if (res.duration() > slowest)
+//                slowest = res.duration();
+//            if (res.result.header.errorCode === "OK")
+//                ++okCalls;
+//            if (okCalls === 10)
+//            {
+//                console.log(util.format(
+//                        "Ok calls:%s fastest:%sms slowest:%sms", okCalls,
+//                        fastest, slowest));
+//                session.close();
+//            }
+//        });
+//    }
+//    // session.close();
+//});
+//
+//session.on("loggedOut", function(res) {
+//    console.log("got loggedOut event");
+//    process.exit(1);
+//});
