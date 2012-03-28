b045d09ea67b592ba65423cf21854c93e092c1e3
Anton <anton.zem@gmail.com>
2012-03-28 11:38:58 +0400

    Added getMarket invocation

--- test/get_market.js
+++ test/get_market.js
@@ -12,6 +12,7 @@ account.password = account.password || 'password';
 
 var betfairSport = require('../index.js');
 var session = betfairSport.openSession(account.login, account.password);
+var marketId;
 
 async.series({
     // Login to Betfair
@@ -27,7 +28,7 @@ async.series({
         });
     },
 
-    // invoke getAllMArkets at uk exchange for soccer
+    // invoke getAllMArkets and get the first tennis 'Match Odds'
     getAllMarkets : function(cb) {
         console.log('Get available tennis matches');
 
@@ -45,9 +46,32 @@ async.series({
                 market = res.result.marketData[index];
                 if (market.marketName != 'Match Odds')
                     continue;
-                var path = market.menuPath.replace(/\\Tennis\\Group A\\/g, '')
-                console.log(path);
+                marketId = market.marketId;
+                break;
             }
+            cb(null, "OK");
+        });
+    },
+    
+    // invoke getMarket on the single market
+    getMarket : function(cb)
+    {
+        console.log('Call getMarket for marketId="%s"', marketId);
+        var inv = session.getMarket(marketId);
+        inv.execute(function(err, res) {
+            console.log('action:', res.action, 'error:', err, 'duration:', res
+                    .duration() / 1000);
+            if (err) {
+                cb("Error in getMarket", null);
+            }
+            //console.log( util.inspect(res.result, false, 10) );
+            console.log("marketId:", res.result.market.marketId);
+            console.log("market name:", res.result.market.name);
+            console.log("market time:", res.result.market.marketTime);
+            console.log("\tplayerOneId:", res.result.market.runners[0].selectionId);
+            console.log("\tplayerOneName:", res.result.market.runners[0].name);
+            console.log("\tplayerTwoId:", res.result.market.runners[1].selectionId);
+            console.log("\tplayerTwoName:", res.result.market.runners[1].name);
             cb(null, "OK");
         });
     },
