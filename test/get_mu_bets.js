478e8fd4d5cd3251a2551dc9d7a1c9df6d7ca675
BetfairTrader <anton.zem@gmail.com>
2012-03-31 15:24:17 +0400

    getMUBets test added

---
+++ test/get_mu_bets.js
@@ -0,0 +1,87 @@
+var https = require('https');
+var fs = require('fs');
+var util = require('util');
+var async = require('async')
+
+var account = JSON.parse(fs.readFileSync('/etc/bf/account.json'));
+
+https.globalAgent.maxSockets = 5;
+
+account.login = account.login || 'nobody';
+account.password = account.password || 'password';
+
+var betfairSport = require('../index.js');
+var session = betfairSport.openSession(account.login, account.password);
+var marketId;
+
+async.series({
+    // Login to Betfair
+    login : function(cb) {
+        console.log('Logging in to Betfair...');
+        session.open(function onLoginFinished(err, res) {
+            if (err) {
+                console.log('Login error', err);
+                process.exit(-1);
+            }
+            console.log('Logged in OK');
+            cb(null, "OK");
+        });
+    },
+
+    // invoke getAllMArkets and get the first tennis 'Match Odds'
+    getAllMarkets : function(cb) {
+        console.log('Get available tennis matches');
+
+        // eventTypeIds 1-soccer, 2-tennis
+        var inv = session.getAllMarkets({
+            eventTypeIds : [ 2 ]
+        });
+        inv.execute(function(err, res) {
+            console.log('action:', res.action, 'error:', err, 'duration:', res
+                    .duration() / 1000);
+            if (err) {
+                cb("Error in getAllMarkets", null);
+            }
+            for ( var index in res.result.marketData) {
+                market = res.result.marketData[index];
+                if (market.marketName != 'Match Odds')
+                    continue;
+                marketId = market.marketId;
+                break;
+            }
+            cb(null, "OK");
+        });
+    },
+
+    // BetfairSession.prototype.getMUBets = function(betStatus, orderBy, count,
+    // sortOrder, startRecord, optional) { // invoke getMUBets on the single
+	// market
+    getMUBets : function(cb)
+    {
+        console.log('Call getMUBets for marketId="%s"', marketId);
+        var inv = session.getMUBets("MU", "NONE", 200, "ASC", 0, {marketId:marketId});
+        inv.execute(function(err, res) {
+            console.log('action:', res.action, 'error:', err, 'duration:', res
+                    .duration() / 1000);
+            if (err) {
+                cb("Error in getMUBets", null);
+            }
+            //console.log( util.inspect(res.result, false, 10) );
+            console.log("errorCode:", res.result.errorCode, 
+            		"recCount", res.result.totalRecordCount );
+            for(var record in res.result.bets)
+                console.log( record );
+            	
+            cb(null, "OK");
+        });
+    },
+
+    // Logout from Betfair
+    logout : function(cb) {
+        console.log('Logging out...');
+        session.close(function(err, res) {
+            console.log('Logged out OK');
+            cb(null, "OK");
+        });
+    }
+});
