fca54ce7208352741f99b7f18cbb85762942fce8
BetfairTrader <anton.zem@gmail.com>
2012-04-02 21:20:54 +0400

    Session API cleanup

--- index.js
+++ index.js
@@ -9,44 +9,6 @@
 //   closeSession - close Betfair Session
 //   getSession   - get Betfair Session
 
-exports.openSession  = openSession;
-exports.closeSession = closeSession;
-exports.getSession   = getSession;
-
 var betfairSession = require("./lib/betfair_session");
-var openSessions = [];
-
-function openSession(login, password)
-{
-    var session = betfairSession.newSession(login, password);
-    openSessions.push(session);
-    return session;
-}
-
-function closeSession(session)
-{
-    var session = openSessions[index];
-    session.logout();
-    return;
-}
-
-function closeAllSessions(session)
-{
-    for(var session in openSessions)
-    {
-        session.close();
-    }
-    return;
-}
-
-function numberOfSessions()
-{
-    return openSessions.length;
-}
-
-function getSession(index)
-{
-    var session = openSessions[index];
-    return session;
-}
+exports.newSession  = betfairSession.newSession;
 
